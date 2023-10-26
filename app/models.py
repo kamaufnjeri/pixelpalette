65a7e92693227c51e32a0abcca5c13b8acd1aa1d
uelkariuki <cratzuel@gmail.com>
2023-10-26 18:49:50 +0300

    Add ShoppingCart db and cart routes

--- app/models.py
+++ app/models.py
@@ -14,14 +14,14 @@ class User(db.Model, UserMixin):
     category = db.Column(db.String(30), nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
     owner_artworks = db.relationship('Artwork', backref=('owner'), lazy=True)
-    
+
     def check_password_correct(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
-    
+
     def __repr__(self):
         # Change printing of objects of class User to help in debugging
         return f'User {self.username}'
-    
+
 
 class Artwork(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -31,6 +31,18 @@ class Artwork(db.Model):
     category = db.Column(db.String(), index=True, nullable=False)
     artwork_url = db.Column(db.String(), unique=True, index=True, nullable=False)
     owner_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+
+class ShoppingCart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    artwork_id = db.Column(db.Integer, db.ForeignKey('artwork.id'), nullable=False)
+    category = db.Column(db.String(), index=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    purchase_date = db.Column(db.TIMESTAMP)
+     # Add a relationship to access the associated artwork
+    artwork = db.relationship('Artwork', backref='cart_items')
+
 
     def __repr__(self):
         # Change printing of objects of class User to help in debugging
